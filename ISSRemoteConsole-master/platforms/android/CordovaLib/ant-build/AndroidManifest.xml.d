b3cfc93e802e663d05f694f8e44416c765a7bb8a
Lorenzo Pontellini <lorenzo.pontellini@studio.unibo.it>
2015-07-13 21:55:52 +0200

    no message

--- ISSRemoteConsole-master/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/edoardo/Desktop/RemoteConsole/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/edoardo/Desktop/RemoteConsole/platforms/android/CordovaLib/AndroidManifest.xml \
--- ISSRemoteConsole-master/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/edoardo/Desktop/RemoteConsole/platforms/android/ant-build/AndroidManifest.xml : \
-/home/edoardo/Desktop/RemoteConsole/platforms/android/AndroidManifest.xml \
